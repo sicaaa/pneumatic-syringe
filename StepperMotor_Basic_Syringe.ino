07bb048451ce539f609c39ce0c3f5a346b171f5d
Jessica Broscheit <contact@jessicabroscheit.com>
2025-09-04 12:36:22 +0200

    StepperMotor_Basic_Syringe.ino

--- StepperMotor_Basic_Syringe.ino
+++ StepperMotor_Basic_Syringe.ino
@@ -1,21 +1,32 @@
-// https://lastminuteengineers.com/28byj48-stepper-motor-arduino-tutorial/
+/* 
+https://lastminuteengineers.com/28byj48-stepper-motor-arduino-tutorial/
+ULN2003 driver board -> Arduino 
 
-//Includes the Arduino Stepper Library
+IN1 -> PIN 8 
+IN2 -> PIN 9 
+IN3 -> PIN 10 
+IN4 -> PIN 11 
+5V  -> 5V
+GND -> GND
+
+*/
+
+
+//Include the Arduino Stepper Library
 #include <Stepper.h>
 
-// Defines the number of steps per rotation 2048
+// Define the number of steps per rotation 2048
 const int stepsPerRevolution = 2048;
 
-// Creates an instance of stepper class
+// Create an instance of stepper class
 // Pins entered in sequence IN1-IN3-IN2-IN4 for proper step sequence
 Stepper myStepper = Stepper(stepsPerRevolution, 8, 10, 9, 11);
 
 void setup() {
-  // Nothing to do (Stepper Library sets pins as outputs)
 }
 
 void loop() {
-  // Rotate CW at 5, 10, 15 RPM
+  // Rotate CW at 5, 10 or 15 RPM
   myStepper.setSpeed(10);
   myStepper.step(2 * stepsPerRevolution); // 3 full turns
   delay(500);
